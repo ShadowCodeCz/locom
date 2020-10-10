0f25c880036f67059fa1fe8d40bc5e41f2eb83b0
Honzin <xsedaj00@gmail.com>
2020-10-10 08:30:29 +0200

    v0.3.10

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ description = "Log Comments"
 
 setup(
     name='locom',
-    version='0.3.9',
+    version='0.3.10',
     packages=find_packages(),
     package_data={
         "locom": ["template/*.html"],
